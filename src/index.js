4433a7e61fd8e10fd94e4545438756a2b296a83c
oliver <olivercher1000@gmail.com>
2024-10-07 00:10:36 +0300

    adds files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+document.querySelector("form").addEventListener("submit",(e) => {
+  e.preventDefault()
+
+   const taskDescription = e.target['new-task-description'].value;
+
+   buildToDo(taskDescription);
+  console.log(e.target['new-task-description'].value);
+  
+})
+function buildToDo(task){
+  const taskItem = document.createElement('li')
+  let p = document.createElement('p')
+  p.textContent = task;
+  let btn = document.createElement('button')
+  btn.addEventListener("click",handleDelete)
+  btn.textContent = 'X';
+  
+  taskItem.appendChild(p);
+  p.appendChild(btn);
+  console.log(p);
+  document.querySelector('#tasks').appendChild(taskItem);
+}
+buildToDo();
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
